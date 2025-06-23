adabcae524c003dd95da39f7d1593febb3eb6172
malnadhudga <gourish.hegde@zop.dev>
2025-06-23 14:15:47 +0530

    Save work before switch

--- assign7/assign_7.go
+++ assign7/assign_7.go
@@ -1,1 +1,63 @@
-package assign7
+package main
+
+import "fmt"
+
+// Shape is an interface that defines the Area method .
+type Shape interface {
+	Area() float64
+}
+
+// rectangle represents a rectangle with breadth (b) and length (l).
+type rectangle struct {
+	b, l float64
+}
+
+// square represents a square with a side length (l).
+type square struct {
+	l float64
+}
+
+// Area calculates the area of a square.
+func (s square) Area() float64 {
+	return s.l * s.l
+}
+
+// Area calculates the area of a rectangle.
+func (r rectangle) Area() float64 {
+	return r.l * r.b
+}
+
+// calArea takes any Shape and returns its area.
+func calArea(s Shape) float64 {
+	return s.Area()
+}
+
+func costs(s Shape) float64 {
+	switch s.(type) {
+	case square:
+		return s.Area() * 100
+	case rectangle:
+		return s.Area() * 20
+	default:
+		return s.Area()
+	}
+}
+
+func main() {
+
+	rect := rectangle{b: 10, l: 20}
+	// Calculate the area of the rectangle.
+	rectArea := calArea(rect)
+	fmt.Printf("Area of rectangle: %.2f\n", rectArea)
+
+	// Create a new square with side length 4.
+	sq := square{l: 10}
+	squareArea := calArea(sq)
+	fmt.Printf("Area of square: %.2f\n", squareArea)
+
+	cost := costs(sq)
+	fmt.Printf("Cost of square: %.2f\n", cost)
+
+	cost = costs(rect)
+	fmt.Printf("Cost of recct: %.2f\n", cost)
+}
